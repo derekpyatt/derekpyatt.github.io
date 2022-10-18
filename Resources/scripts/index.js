52a68a67b7554cb1ff49710a5770fe8cd59b4708
derekpyatt <derekpyatt2020@gmail.com>
2022-10-17 20:16:05 -0600

    Old site purge

--- Resources/scripts/index.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-    const hamburgerMenu = $('#hamburger-menu');
-    const menuItems = $('ul.nav>li[id!="hamburger-menu"]');
-    console.log(menuItems);
-    hamburgerMenu.on('click', function () {
-        menuItems.toggleClass('hiddenOnMobile');
-    });
-});
